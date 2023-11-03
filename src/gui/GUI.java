a9335c2cfa1e5d184b53de8486f7ad7a7ffc1c0c
Leonhard Leung <Leonhardleung@gmail.com>
2023-11-04 00:05:23 +0800

    Added the frame of the GUI

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,2 +1,19 @@
-package gui;public class Frame {
+package gui;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    public GUI() {
+        initializeFrame();
+    }
+
+    private void initializeFrame() {
+        this.setLayout(null);
+        this.setTitle("Huffman Codec Application");
+        this.setSize(1200, 800);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
 }
