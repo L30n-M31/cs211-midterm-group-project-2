72e04114e7ed0c2d8a25c5813e4abcd981bfcb18
Leonhard Leung <Leonhardleung@gmail.com>
2023-11-04 00:04:37 +0800

    Added the gui executable

--- src/app/HuffmanApp.java
+++ src/app/HuffmanApp.java
@@ -1,2 +1,32 @@
-package app;public class HuffmanApp {
+package app;
+
+import gui.GUI;
+import gui.MainPanel;
+import gui.SidePanel;
+import util.HuffmanCodec;
+
+import javax.swing.*;
+
+public class HuffmanApp {
+    private final HuffmanCodec huffmanOperations = new HuffmanCodec();
+
+    public static void main(String[] args) {
+        HuffmanApp program;
+        try {
+            program = new HuffmanApp();
+            program.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error detected");
+        }
+    }
+
+    public void run() {
+        SidePanel sidePanel = new SidePanel(huffmanOperations);
+        MainPanel mainPanel = new MainPanel(huffmanOperations);
+
+        GUI frame = new GUI();
+        frame.add(sidePanel);
+        frame.add(mainPanel);
+    }
 }
